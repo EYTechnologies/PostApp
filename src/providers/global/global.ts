945155272d8f22ecbf37b74faa87f06c2d22be83
ayush89able <ayush89able@eyt.ee>
2018-04-16 16:26:45 +0530

    tabs added

---
+++ src/providers/global/global.ts
@@ -0,0 +1,24 @@
+
+import { Injectable } from '@angular/core';
+
+/*
+  Generated class for the GlobalProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class GlobalProvider {
+
+  userID:string='';
+  postID:string='';
+  userNAME:string='';
+  allPosts:any;
+  myPosts:any;
+
+  constructor() {
+    console.log('Hello GlobalProvider Provider');
+    console.log(this.userID);
+  }
+
+}
